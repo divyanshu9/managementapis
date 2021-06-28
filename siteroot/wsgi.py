6e0d70cdf50961c7c06dffb99b9ab56f1649f3d0
Divyanshu Mishra <divyanshu@prismberry.com>
2021-06-28 22:40:58 +0530

    fixed redirecting

--- siteroot/wsgi.py
+++ siteroot/wsgi.py
@@ -1,9 +1,6 @@
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 APP_NAME = 'siteroot.conf'
 os.environ['DJANGO_SETTINGS_MODULE'] = APP_NAME + '.settings'
 
