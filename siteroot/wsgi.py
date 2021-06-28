1f5e71897e38618419edf6100449105d0a6cbb49
Divyanshu Mishra <divyanshu@prismberry.com>
2021-06-28 22:19:46 +0530

    added whiter noise

--- siteroot/wsgi.py
+++ siteroot/wsgi.py
@@ -2,8 +2,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 APP_NAME = 'siteroot.conf'
 os.environ['DJANGO_SETTINGS_MODULE'] = APP_NAME + '.settings'
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
