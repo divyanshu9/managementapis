ee4684846030f65177e0dca07ac627854d4bc140
Divyanshu Mishra <divyanshu@prismberry.com>
2021-07-15 15:31:54 +0530

    Bd

--- siteroot/conf/local_settings.py
+++ siteroot/conf/local_settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'siteroot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'projectmanagement',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
