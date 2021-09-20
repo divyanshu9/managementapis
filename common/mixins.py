3995c5874c13a05e8b2035a323696e2fb170e534
myusername <mishradivyanshu05@gmail.com>
2021-09-20 19:32:17 +0530

    fix

--- common/mixins.py
+++ common/mixins.py
@@ -15,24 +15,25 @@ class TrackableMixin(models.Model):
 
 
 class APIKEYMixin:
+    pass
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        data['api_key'] = request.META['HTTP_AUTHORIZATION']
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        queryset = queryset.filter(api_key=request.META['HTTP_AUTHORIZATION'])
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    # def create(self, request, *args, **kwargs):
+    #     data = request.data
+    #     data['api_key'] = request.META['HTTP_AUTHORIZATION']
+    #     serializer = self.get_serializer(data=data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    #
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     queryset = queryset.filter(api_key=request.META['HTTP_AUTHORIZATION'])
+    #
+    #     page = self.paginate_queryset(queryset)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(serializer.data)
+    #
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
